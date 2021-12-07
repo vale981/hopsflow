9a205f35f2b86146aafb04cbcd4cefbd87ef705b
Valentin Boettcher <hiro@protagon.space>
2021-12-07 11:39:32 +0100

    update nix-flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,31 @@
 {
-  description = "A very basic flake";
+  description = "Calculating open system bath energy changes with HOPS and analytically. ";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
-
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    mach-nix.url = "github:DavHau/mach-nix";
   };
+
+  outputs = { self, nixpkgs, flake-utils, mach-nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        python = "python39";
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        mach-nix-wrapper = import mach-nix { inherit pkgs python; };
+        requirements = builtins.readFile ./requirements.txt;
+        pythonBuild = mach-nix-wrapper.mkPython { inherit requirements; };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            # dev packages
+            (pkgs.${python}.withPackages
+              (ps: with ps; [ black mypy sphinx-autodoc-typehints sphinx pydata-sphinx-theme  ])) # <--- change here
+            pkgs.nodePackages.pyright
+
+            # app packages
+            pythonBuild
+          ];
+        };
+      });
 }
