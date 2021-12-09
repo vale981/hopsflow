d6fa5b6c68c4db36be8dd79fb0de9c3123a846eb
Valentin Boettcher <hiro@protagon.space>
2021-12-09 14:59:46 +0100

    switch to poetry

--- flake.nix
+++ flake.nix
@@ -3,61 +3,15 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    mach-nix.url = "github:DavHau/mach-nix";
-    flake-utils.url = "github:numtide/flake-utils";
-    fcSpline.url = "github:vale981/fcSpline";
-    stocproc.url = "github:vale981/stocproc";
+    utils.url = "github:vale981/hiro-flake-utils";
   };
 
-   outputs = { self, nixpkgs, flake-utils, mach-nix, fcSpline, stocproc }:
-     let
-       python = "python39";
-       devShell = pkgs:
-         pkgs.mkShell {
-           buildInputs = [
-             (pkgs.${python}.withPackages
-               (ps: with ps; [ black mypy ]))
-             pkgs.nodePackages.pyright
-           ];
-         };
-
-     in flake-utils.lib.eachSystem ["x86_64-linux"] (system:
-       let
-         pkgs = nixpkgs.legacyPackages.${system};
-         mach-nix-wrapper = import mach-nix { inherit pkgs python; };
-
-
-
-         requirements = builtins.readFile ./requirements.txt;
-         fcSplinePkg = fcSpline.defaultPackage.${system};
-         stocprocPkg = stocproc.defaultPackage.${system};
-
-         hopsflow  = mach-nix-wrapper.buildPythonPackage rec {
-           src = ./.;
-           requirements = ''
-           numpy >= 1.20
-           scipy >= 1.6
-           h5py
-           tqdm
-           lmfit
-           '';
-           propagatedBuildInputs = [stocprocPkg fcSplinePkg];
-         };
-
-         pythonShell = mach-nix-wrapper.mkPythonShell {
-           requirements = requirements;
-           packagesExtra = [hopsflow];
-         };
-
-         mergeEnvs = envs:
-           pkgs.mkShell (builtins.foldl' (a: v: {
-             buildInputs = a.buildInputs ++ v.buildInputs;
-             nativeBuildInputs = a.nativeBuildInputs ++ v.nativeBuildInputs;
-           }) (pkgs.mkShell { }) envs);
-
-       in {
-         devShell = mergeEnvs [ (devShell pkgs) pythonShell ];
-         defaultPackage = hopsflow;
-         packages.hopsflow = hopsflow;
-       });
+  outputs = { self, utils, nixpkgs, ... }:
+    (utils.lib.poetry2nixWrapper nixpkgs {
+      name = "hopsflow";
+      shellPackages = pkgs: with pkgs; [ black pyright ];
+      poetryArgs = {
+        projectDir = ./.;
+      };
+    });
 }
